e11962de920276193ad46e6e240aae389f87da18
Egor <egor21chuv@yandex.ru>
2023-12-05 18:09:54 +0300

    Added video

--- src/test/java/tests/TestBaseExtended.java
+++ src/test/java/tests/TestBaseExtended.java
@@ -47,6 +47,6 @@ public class TestBaseExtended {
         Attach.screenshotAs("Last screenshot");
         Attach.pageSource();
         Attach.browserConsoleLogs();
-        // Attach.video();
+        Attach.addVideo();
     }
 }
